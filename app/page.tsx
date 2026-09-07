d362bfe4f4c89b9e896716dc404dc21d36ee74c0
Nicolas Llanos <54457809+NicolasLlanos8799@users.noreply.github.com>
2026-09-07 11:03:12 +0200

    Wrap homepage with language provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
+import { LanguageProvider } from "@/components/i18n/LanguageProvider";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { Hero } from "@/components/sections/Hero";
 import { WhatWeDo } from "@/components/sections/WhatWeDo";
 
 export default function Home() {
   return (
-    <>
+    <LanguageProvider>
       <SiteHeader />
       <main>
         <Hero />
         <WhatWeDo />
       </main>
-    </>
+    </LanguageProvider>
   );
 }
