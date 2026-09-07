3f6d0d4914031f2b3f896916f2be41c22f8eede2
Nicolas Llanos <54457809+NicolasLlanos8799@users.noreply.github.com>
2026-09-07 11:03:07 +0200

    Add bilingual language provider

---
+++ components/i18n/LanguageProvider.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+export type Language = "en" | "es";
+
+type LanguageContextValue = {
+  language: Language;
+  setLanguage: (language: Language) => void;
+};
+
+const LanguageContext = createContext<LanguageContextValue | undefined>(undefined);
+
+export function LanguageProvider({ children }: { children: React.ReactNode }) {
+  const [language, setLanguage] = useState<Language>("en");
+
+  useEffect(() => {
+    const savedLanguage = window.localStorage.getItem("leba-language");
+    if (savedLanguage === "en" || savedLanguage === "es") {
+      setLanguage(savedLanguage);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("leba-language", language);
+    document.documentElement.lang = language;
+  }, [language]);
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+}
+
+export function useLanguage() {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error("useLanguage must be used inside LanguageProvider");
+  }
+  return context;
+}
