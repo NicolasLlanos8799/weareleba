dc637ce365b050b9e2e10e697c4692c0ca81efa7
Nicolas Llanos <54457809+NicolasLlanos8799@users.noreply.github.com>
2026-09-08 17:49:14 +0200

    Render the new combined LEBA approach section as a single section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { SiteFooter } from "@/components/layout/SiteFooter";
 import { ScrollMotion } from "@/components/system/ScrollMotion";
 import { Hero } from "@/components/sections/Hero";
 import { WhatWeDo } from "@/components/sections/WhatWeDo";
-import { OurProcess } from "@/components/sections/OurProcess";
 import { WeAreLeba } from "@/components/sections/WeAreLeba";
 
 export default function Home() {
@@ -16,7 +15,6 @@ export default function Home() {
         <Hero />
         <WhatWeDo />
         <WeAreLeba />
-        <OurProcess />
       </main>
       <SiteFooter />
     </LanguageProvider>
