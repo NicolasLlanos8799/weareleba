ba0a27248852799b9e06ac90fd119d4229deb059
Nicolas Llanos <54457809+NicolasLlanos8799@users.noreply.github.com>
2026-09-08 17:18:01 +0200

    Place compact We Are LEBA intro before selected experience

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
       <main>
         <Hero />
         <WhatWeDo />
-        <OurProcess />
         <WeAreLeba />
+        <OurProcess />
       </main>
       <SiteFooter />
     </LanguageProvider>
