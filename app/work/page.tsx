f7bb0618acb05628c75502f6a5fa1ee7ae9a1937
Nicolas Llanos <54457809+NicolasLlanos8799@users.noreply.github.com>
2026-09-14 18:20:40 +0200

    Redirect Work page to work in process

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,16 +1,5 @@
-import { LanguageProvider } from "@/components/i18n/LanguageProvider";
-import { SiteHeader } from "@/components/layout/SiteHeader";
-import { SiteFooter } from "@/components/layout/SiteFooter";
-import { ScrollMotion } from "@/components/system/ScrollMotion";
-import { WorkPortfolio } from "@/components/sections/WorkPortfolio";
+import { redirect } from "next/navigation";
 
 export default function WorkPage() {
-  return (
-    <LanguageProvider>
-      <ScrollMotion />
-      <SiteHeader />
-      <WorkPortfolio />
-      <SiteFooter />
-    </LanguageProvider>
-  );
+  redirect("/workinprocess");
 }
