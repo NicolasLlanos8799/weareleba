4485cf6879ae4ddadf1110a0ed550a2beae26db4
Nicolas Llanos <54457809+NicolasLlanos8799@users.noreply.github.com>
2026-09-14 18:24:01 +0200

    Add English route for Studio page

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import { LanguageProvider } from "@/components/i18n/LanguageProvider";
+import { SiteHeader } from "@/components/layout/SiteHeader";
+import { SiteFooter } from "@/components/layout/SiteFooter";
+import { ScrollMotion } from "@/components/system/ScrollMotion";
+import { WhoHelps } from "@/components/sections/WhoHelps";
+
+export default function AboutPage() {
+  return (
+    <LanguageProvider>
+      <ScrollMotion />
+      <SiteHeader tone="dark" />
+      <main>
+        <WhoHelps />
+      </main>
+      <SiteFooter />
+    </LanguageProvider>
+  );
+}
